7e9efef1b33a5f75755bd7c6c41332fb7ca0d142
Stefano Babini <stefano.babini5@studio.unibo.it>
2025-03-26 17:34:31 +0100

    forget shapeDrawer

--- src/main/java/pcd/ass01/BoidPattern/ShapeDrawer.java
+++ src/main/java/pcd/ass01/BoidPattern/ShapeDrawer.java
@@ -3,36 +3,6 @@ package pcd.ass01.BoidPattern;
 import java.awt.*;
 
 public class ShapeDrawer {
-<<<<<<< HEAD
-    private static final int scale = 5;
-
-    public static void drawCircle(Graphics g, int px, int py) {
-        g.fillOval(px, py, 1 * scale, 1 * scale);
-    }
-
-    public static void drawSquare(Graphics g, int px, int py) {
-        g.fillRect(px, py, 1 * scale, 1 * scale);
-    }
-
-    public static void drawTriangle(Graphics g, int px, int py) {
-        int[] xPoints = { px, px + 1 * scale, px - 1 * scale };
-        int[] yPoints = { py - 1 * scale, py + 1 * scale, py + 1 * scale };
-        g.fillPolygon(xPoints, yPoints, 3);
-    }
-
-    public static void drawStar(Graphics g, int px, int py) {
-        int[] starX = { px, px + 5 * scale, px + 10 * scale, px + 7 * scale, px + 3 * scale, 
-                         px, px - 3 * scale, px - 7 * scale, px - 10 * scale, px - 5 * scale };
-        int[] starY = { py - 10 * scale, py - 3 * scale, py - 3 * scale, py + 2 * scale, py + 7 * scale, 
-                         py + 3 * scale, py + 7 * scale, py + 2 * scale, py - 3 * scale, py - 3 * scale };
-        g.fillPolygon(starX, starY, 10);
-    }
-
-    public static void drawDiamond(Graphics g, int px, int py) {
-        int[] diamondX = { px, px + 1 * scale, px, px - 1 * scale };
-        int[] diamondY = { py - 1 * scale, py, py + 1 * scale, py };
-        g.fillPolygon(diamondX, diamondY, 4);
-=======
     private final static double SCALE = 0.6;
     private final static int BORDER_SIZE = 2;   // best values = 1 or 2
 
@@ -92,6 +62,5 @@ public class ShapeDrawer {
             int[] diamondY = {y - s, y, y + s, y};
             gr.fillPolygon(diamondX, diamondY, 4);
         });
->>>>>>> 4b51fe88519783edbca946e05ddd1fb1bb77e885
     }
 }
