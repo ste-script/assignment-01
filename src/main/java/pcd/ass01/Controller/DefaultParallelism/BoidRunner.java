febfca12ab00965c9017880f870d3e5c8b15b361
Norbert Gabos <dont.vip@gmail.com>
2025-03-28 20:33:06 +0100

    reimplemented Multithreadded versio

---
+++ src/main/java/pcd/ass01/Controller/DefaultParallelism/BoidRunner.java
@@ -0,0 +1,48 @@
+package pcd.ass01.Controller.DefaultParallelism;
+
+import pcd.ass01.Model.Boid;
+import pcd.ass01.Model.BoidsModel;
+import pcd.ass01.View.BoidPattern.BoidPatterns;
+
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+
+public class BoidRunner implements Runnable {
+
+    private List<Boid> boidChunk;
+    private BoidsModel model;
+    private CyclicBarrier barrier;
+    private boolean run = true;
+
+    public BoidRunner(List<Boid> boidChunk, BoidsModel model,
+                      CyclicBarrier barrier, BoidPatterns.Pattern boidPattern) {
+        this.boidChunk = boidChunk;
+        this.model = model;
+        this.barrier = barrier;
+        setBoidsPattern(boidPattern);
+    }
+
+    private void setBoidsPattern(BoidPatterns.Pattern pattern) {
+        this.boidChunk.forEach(boid -> boid.setPattern(pattern));
+    }
+
+    public void stop() {
+        run = false;
+    }
+
+    public void run() {
+        while (run) {
+            try {
+                boidChunk.forEach(boid -> boid.updateVelocity(model));
+                barrier.await();
+                boidChunk.forEach(boid -> boid.updatePosition(model));
+                barrier.await();
+                // between these two barriers we check if the number of boids has changed
+                // and if the thread should continue running
+                barrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/pcd/ass01/Controller/DefaultParallelism/BoidsMultithreaded.java
@@ -0,0 +1,126 @@
+package pcd.ass01.Controller.DefaultParallelism;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+
+import pcd.ass01.BoidsSimulation;
+import pcd.ass01.Model.Boid;
+import pcd.ass01.Model.BoidsModel;
+import pcd.ass01.View.BoidPattern.BoidPatterns;
+
+public class BoidsMultithreaded {
+    private List<BoidRunner> boidRunners;
+    private CyclicBarrier barrier;
+    private BoidsModel model;
+    private int numberOfThreads;
+    private BoidPatterns boidPatterns = new BoidPatterns();
+    private boolean startMode;
+
+    public BoidsMultithreaded(BoidsModel model) {
+        this.model = model;
+        boidRunners = new ArrayList<>();
+        createAndAssignBoidRunners();
+        startMode = false;
+    }
+
+    public synchronized void start() {
+        boidRunners.forEach(boidRunner -> Thread.ofPlatform().start(boidRunner));
+    }
+
+    public synchronized void update() {
+        if (model.isSuspended()) {
+            return;
+        }
+        this.updateVelocity();
+        this.updatePosition();
+        this.checkThreadValidity();
+        this.checkModeChanged();
+    }
+
+    private void checkModeChanged() {
+        /*if (startMode != BoidsSimulation.getPatternBased()) {
+            startMode = BoidsSimulation.getPatternBased();
+            redistributeBoids();
+        }*/
+    }
+
+    private void calculateNumberOfThreads() {
+        var numberOfAvailableProcessors = Runtime.getRuntime().availableProcessors() + 1 ;
+        /*if (BoidsSimulation.getPatternBased()) {
+            numberOfAvailableProcessors = BoidsSimulation.THREAD_COUNT;
+        }*/
+        numberOfThreads = Math.max(1, Math.min(numberOfAvailableProcessors, model.getBoids().size()));
+    }
+
+    private void createAndAssignBoidRunners() {
+        calculateNumberOfThreads();
+        this.barrier = new CyclicBarrier(numberOfThreads + 1);
+        final var boids = model.getBoids();
+        var chunkSize = Math.max(1, boids.size() / numberOfThreads);
+        var boidsGroupedInChunks = getBoidsGroupedInChunks(boids, numberOfThreads, chunkSize);
+
+        // assigning patterns to each BoidRunner
+        this.boidPatterns.resetPatterns();
+        boidsGroupedInChunks.forEach((boidChunk) -> {
+            BoidPatterns.Pattern assignedPattern = BoidsSimulation.DEFAULT_PATTERN;
+            /*if (BoidsSimulation.getPatternBased()) {
+                assignedPattern = this.boidPatterns.getNextPattern();
+            }*/
+            boidRunners.add(new BoidRunner(boidChunk, model, barrier, assignedPattern));
+        });
+    }
+
+    private void checkThreadValidity() {
+        try {
+            if (model.getNumberOfBoids() != model.getBoids().size()) {
+                redistributeBoids();
+            }
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteThreads() {
+        boidRunners.stream().forEach(BoidRunner::stop);
+        boidRunners.clear();
+    }
+
+    private synchronized void redistributeBoids() {
+        deleteThreads();
+        model.setBoids(model.getNumberOfBoids());
+        createAndAssignBoidRunners();
+        start();
+    }
+
+    private ArrayList<List<Boid>> getBoidsGroupedInChunks(final List<Boid> boids, final int numberOfThreads,
+                                                          int chunkSize) {
+        var boidsGroupedInChunks = new ArrayList<List<Boid>>();
+        for (int i = 0; i < numberOfThreads; i++) {
+            var start = i * chunkSize;
+            var end = Math.min((i + 1) * chunkSize, boids.size());
+            if (i == numberOfThreads - 1) {
+                end = boids.size();
+            }
+            boidsGroupedInChunks.add(boids.subList(start, end));
+        }
+        return boidsGroupedInChunks;
+    }
+
+    private void updateVelocity() {
+        try {
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updatePosition() {
+        try {
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
