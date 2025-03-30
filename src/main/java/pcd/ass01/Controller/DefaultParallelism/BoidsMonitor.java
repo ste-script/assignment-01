d35fbf8f1c9783ff5bf7c2a2f1d6d779ec338a1c
Stefano Babini <stefano.babini5@studio.unibo.it>
2025-03-30 16:33:35 +0200

    fix monitor

--- src/main/java/pcd/ass01/Controller/DefaultParallelism/BoidsMonitor.java
+++ src/main/java/pcd/ass01/Controller/DefaultParallelism/BoidsMonitor.java
@@ -3,22 +3,32 @@ package pcd.ass01.Controller.DefaultParallelism;
 public class BoidsMonitor {
     private final int numberOfThreads;
     private int threadWaiting;
+    private long iteration;
 
     public BoidsMonitor(int numberOfThreads) {
         this.numberOfThreads = numberOfThreads;
         this.threadWaiting = 0;
+        this.iteration = 0;
     }
 
     public synchronized void await() {
         threadWaiting++;
-        if (this.numberOfThreads - this.threadWaiting == 0) {
+        if (this.numberOfThreads == this.threadWaiting) {
+            iteration++;
+            threadWaiting = 0;
             notifyAll();
+            return;
         }
-        try {
-            wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (true) {
+            var iteration = this.iteration;
+            try {
+                wait();
+                if (iteration != this.iteration) {
+                    return;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        threadWaiting--;
     }
 }
