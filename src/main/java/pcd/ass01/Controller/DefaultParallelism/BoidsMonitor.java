997ee09e6e8105a05ed6051cb8a94576dd1205aa
Stefano Babini <stefano.babini5@studio.unibo.it>
2025-03-30 16:35:35 +0200

    fix names

--- src/main/java/pcd/ass01/Controller/DefaultParallelism/BoidsMonitor.java
+++ src/main/java/pcd/ass01/Controller/DefaultParallelism/BoidsMonitor.java
@@ -2,20 +2,20 @@ package pcd.ass01.Controller.DefaultParallelism;
 
 public class BoidsMonitor {
     private final int numberOfThreads;
-    private int threadWaiting;
+    private int numberOfThreadWating;
     private long iteration;
 
     public BoidsMonitor(int numberOfThreads) {
         this.numberOfThreads = numberOfThreads;
-        this.threadWaiting = 0;
+        this.numberOfThreadWating = 0;
         this.iteration = 0;
     }
 
     public synchronized void await() {
-        threadWaiting++;
-        if (this.numberOfThreads == this.threadWaiting) {
+        numberOfThreadWating++;
+        if (this.numberOfThreads == this.numberOfThreadWating) {
             iteration++;
-            threadWaiting = 0;
+            numberOfThreadWating = 0;
             notifyAll();
             return;
         }
